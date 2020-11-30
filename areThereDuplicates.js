f16098be1d596b17d466a491e5be82d0e4450b5a
caldric <cvaldric@gmail.com>
2020-11-30 13:07:45 -0500

    Solve areThereDuplicates using frequency pattern

---
+++ areThereDuplicates.js
@@ -0,0 +1,15 @@
+function areThereDuplicates(...values) {
+  const seenValues = {};
+  for (const elem of values) {
+    if (elem in seenValues) {
+      return true;
+    } else {
+      seenValues[elem] = true;
+    }
+  }
+  return false;
+}
+
+console.log(areThereDuplicates(1, 2, 3));
+console.log(areThereDuplicates(1, 2, 2));
+console.log(areThereDuplicates('a', 'b', 'c', 'a'));
