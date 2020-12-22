2ed83447609c6d2cd4ce83037ed4b31d841f00d8
caldric <cvaldric@gmail.com>
2020-12-22 17:36:14 -0500

    Write a recursive reverse string function

---
+++ reverse.ts
@@ -0,0 +1,18 @@
+// Write a recursive function called reverse which accepts a string and returns a new string in reverse
+
+function reverse(str: string): string {
+  // Base case
+  if (str.length <= 1) {
+    return str;
+  }
+
+  // Recursion
+  const lastChar: string = str[str.length - 1];
+  const newStr: string = str.slice(0, str.length - 1);
+  return lastChar + reverse(newStr);
+}
+
+console.log(reverse('awesome')); // emosewa
+console.log(reverse('rithmschool')); // loohcsmhtir
+console.log(reverse('')); // ''
+console.log(reverse('a')); // a
