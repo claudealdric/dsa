a8c2f0a14986a7ce5cbaef1c50b942aed30c123b
caldric <cvaldric@gmail.com>
2020-12-22 21:04:42 -0500

    Rewrite sumZero in TypeScript

--- sumZero.ts
+++ sumZero.ts
@@ -1,17 +1,20 @@
 // Write a function called sum_zero that accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist.
 
-function sumZero(intArray, targetSum = 0) {
+function sumZero(
+  intArray: number[],
+  targetSum: number = 0
+): [number, number] | undefined {
   // Approach: have two pointers and go from the outside in
 
   // Assign the left and right pointers (indices)
-  let leftPointer = 0;
-  let rightPointer = intArray.length - 1;
+  let leftPointer: number = 0;
+  let rightPointer: number = intArray.length - 1;
 
   // Compare the two numbers (until the two pointers meet)
   while (leftPointer < rightPointer) {
-    const leftNumber = intArray[leftPointer];
-    const rightNumber = intArray[rightPointer];
-    const currentSum = leftNumber + rightNumber;
+    const leftNumber: number = intArray[leftPointer];
+    const rightNumber: number = intArray[rightPointer];
+    const currentSum: number = leftNumber + rightNumber;
 
     if (currentSum === targetSum) {
       // If the sum is equal to the target, return the pair
