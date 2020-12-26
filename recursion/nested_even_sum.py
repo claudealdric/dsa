89159307fe1f1be111c1fb38766b6aa0bd60fb0c
caldric <cvaldric@gmail.com>
2020-12-26 12:43:41 -0500

    Reformat using Black

--- recursion/nested_even_sum.py
+++ recursion/nested_even_sum.py
@@ -8,25 +8,26 @@ def nested_even_sum(dictionary, sum=0):
             sum += nested_even_sum(value)
     return sum
 
+
 # Tests
 obj1 = {
-  'outer': 2,
-  'obj': {
-    'inner': 2,
-    'otherObj': {
-      'superInner': 2,
-      'notANumber': True,
-      'alsoNotANumber': 'yup',
+    "outer": 2,
+    "obj": {
+        "inner": 2,
+        "otherObj": {
+            "superInner": 2,
+            "notANumber": True,
+            "alsoNotANumber": "yup",
+        },
     },
-  },
 }
 
 obj2 = {
-  'a': 2,
-  'b': { 'b': 2, 'bb': { 'b': 3, 'bb': { 'b': 2 } } },
-  'c': { 'c': { 'c': 2 }, 'cc': 'ball', 'ccc': 5 },
-  'd': 1,
-  'e': { 'e': { 'e': 2 }, 'ee': 'car' },
+    "a": 2,
+    "b": {"b": 2, "bb": {"b": 3, "bb": {"b": 2}}},
+    "c": {"c": {"c": 2}, "cc": "ball", "ccc": 5},
+    "d": 1,
+    "e": {"e": {"e": 2}, "ee": "car"},
 }
 
 print(nested_even_sum(obj1) == 6)
