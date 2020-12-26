3705255426442839ff7034e7609c7502a38a1406
caldric <cvaldric@gmail.com>
2020-12-26 11:55:44 -0500

    Reformat with new Prettier config

--- recursion/flatten.ts
+++ recursion/flatten.ts
@@ -1,22 +1,22 @@
 // Write a recursive function called flatten which accepts an array of arrays and returns a new array with all values flattened.
 
-type ArrayOfArrays = Array<number | ArrayOfArrays>;
+type ArrayOfArrays = Array<number | ArrayOfArrays>
 
 function flatten(arr: ArrayOfArrays) {
-  let flattenedArr: number[] = [];
+  let flattenedArr: number[] = []
 
   for (const elem of arr) {
     if (typeof elem === 'number') {
-      flattenedArr.push(elem);
+      flattenedArr.push(elem)
     } else if (Array.isArray(elem)) {
-      flattenedArr = flattenedArr.concat(flatten(elem));
+      flattenedArr = flattenedArr.concat(flatten(elem))
     }
   }
 
-  return flattenedArr;
+  return flattenedArr
 }
 
-console.log(flatten([1, 2, 3, [4, 5]])); // [1, 2, 3, 4, 5]
-console.log(flatten([1, [2, [3, 4], [[5]]]])); // [1, 2, 3, 4, 5]
-console.log(flatten([[1], [2], [3]])); // [1,2,3]
-console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])); // [1,2,3]
+console.log(flatten([1, 2, 3, [4, 5]])) // [1, 2, 3, 4, 5]
+console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
+console.log(flatten([[1], [2], [3]])) // [1,2,3]
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3]
