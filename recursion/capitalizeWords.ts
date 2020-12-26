b5741b381075fd15955bcbeb8668cec916c58bcd
caldric <cvaldric@gmail.com>
2020-12-26 12:21:53 -0500

    Solve recursive string array converter using TS

---
+++ recursion/capitalizeWords.ts
@@ -0,0 +1,23 @@
+// Write a recursive function called capitalizeWords. Given an array of words, return a new array containing each word capitalized.
+import _ from 'lodash'
+
+function capitalizeWords(arr: string[]): string[] {
+  // Base cases
+  if (arr.length === 1) return [arr[0].toUpperCase()]
+  if (arr.length === 0) return []
+
+  // Recursion
+  const currentStr: string = arr[0]
+  const newStr: string = currentStr.toUpperCase()
+  return [newStr].concat(capitalizeWords(arr.slice(1)))
+}
+
+// Tests
+console.log(
+  _.isEqual(capitalizeWords(['what', 'is', 'recursion']), [
+    'WHAT',
+    'IS',
+    'RECURSION',
+  ])
+)
+console.log(_.isEqual(capitalizeWords([]), []))
