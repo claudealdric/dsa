cfe4199753c1c55837e1b9d80c608b0a9e537ef2
caldric <cvaldric@gmail.com>
2021-01-02 14:10:18 -0500

    Red: throws error when arr smaller than length 5 is passed

--- tests/miniMaxSum.spec.ts
+++ tests/miniMaxSum.spec.ts
@@ -12,4 +12,12 @@ describe('miniMaxSum', () => {
     const result = miniMaxSum([1, 3, 5, 7, 9])
     expect(result).to.have.same.members([16, 24])
   })
+
+  it('Throws an error when an array smaller than length 5 is passed in', () => {
+    expect(() => miniMaxSum([1, 2, 3, 4])).to.throw()
+    expect(() => miniMaxSum([1, 2, 3])).to.throw()
+    expect(() => miniMaxSum([1, 2])).to.throw()
+    expect(() => miniMaxSum([1])).to.throw()
+    expect(() => miniMaxSum([])).to.throw()
+  })
 })
