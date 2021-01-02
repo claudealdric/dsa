bc93f431b85664f57ba8f31123c5438a0d215890
caldric <cvaldric@gmail.com>
2021-01-02 13:49:50 -0500

    Refactor: remove redundant tests

--- tests/miniMaxSum.spec.ts
+++ tests/miniMaxSum.spec.ts
@@ -3,10 +3,6 @@ import { expect } from 'chai'
 import { miniMaxSum } from '../miniMaxSum'
 
 describe('miniMaxSum', () => {
-  it('Can be called', () => {
-    miniMaxSum()
-  })
-
   it('Returns an empty array with an empty array as its argument', () => {
     const result = miniMaxSum([])
     expect(result).to.have.same.members([])
