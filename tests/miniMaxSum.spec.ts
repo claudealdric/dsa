ab6e90a1c877121a6def3d6850078fcac0d404c1
caldric <cvaldric@gmail.com>
2021-01-02 13:37:47 -0500

    Red: miniMaxSum can be called

--- tests/miniMaxSum.spec.ts
+++ tests/miniMaxSum.spec.ts
@@ -1,9 +1,9 @@
 import 'mocha'
 import { expect } from 'chai'
-// import { miniMaxSum } from '../miniMaxSum'
+import { miniMaxSum } from '../miniMaxSum'
 
 describe('miniMaxSum', () => {
-  it('Returns true', () => {
-    expect(true).to.equal(true)
+  it('Can be called', () => {
+    miniMaxSum()
   })
 })
