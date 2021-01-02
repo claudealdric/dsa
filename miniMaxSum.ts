2eb98afa862aa1238277492057ae06bf3ed07d02
caldric <cvaldric@gmail.com>
2021-01-02 14:25:45 -0500

    Refactor: throw error when arr.length != 5

--- miniMaxSum.ts
+++ miniMaxSum.ts
@@ -1,12 +1,12 @@
 export const miniMaxSum = (arr: number[] = []): [number, number] => {
-  // Throw error if the array length is smaller than 5
-  if (arr.length < 5) throw new Error('Array length must be at least 5')
+  // Throw error if the array length is not equal to 5
+  if (arr.length !== 5) throw new Error('Array length must be equal to 5')
 
   // Initialize the minSum and maxSum variables
   let minSum: number = Number.POSITIVE_INFINITY
   let maxSum: number = Number.NEGATIVE_INFINITY
 
-  // Find the total sum
+  // Find the total sum of the numbers in the array
   const totalSum: number = arr.reduce((sum, num) => sum + num)
 
   // Iterate through the array
--- tests/miniMaxSum.spec.ts
+++ tests/miniMaxSum.spec.ts
@@ -13,11 +13,14 @@ describe('miniMaxSum', () => {
     expect(result).to.have.same.members([16, 24])
   })
 
-  it('Throws an error when an array smaller than length 5 is passed in', () => {
-    expect(() => miniMaxSum([1, 2, 3, 4])).to.throw()
-    expect(() => miniMaxSum([1, 2, 3])).to.throw()
-    expect(() => miniMaxSum([1, 2])).to.throw()
-    expect(() => miniMaxSum([1])).to.throw()
+  it('Throws an error when array length is not equal to 5', () => {
     expect(() => miniMaxSum([])).to.throw()
+    expect(() => miniMaxSum([1, 2, 3, 4, 5, 6])).to.throw()
+
+    const testArr: number[] = []
+    for (let i = 1; i < 5; ++i) {
+      testArr.push(i)
+      expect(() => miniMaxSum(testArr)).to.throw()
+    }
   })
 })
