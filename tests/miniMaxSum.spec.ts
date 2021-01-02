6142c4995dac415d381de210a958d44d975f3ee1
caldric <cvaldric@gmail.com>
2021-01-02 13:36:00 -0500

    Add sample test

---
+++ tests/miniMaxSum.spec.ts
@@ -0,0 +1,9 @@
+import 'mocha'
+import { expect } from 'chai'
+// import { miniMaxSum } from '../miniMaxSum'
+
+describe('miniMaxSum', () => {
+  it('Returns true', () => {
+    expect(true).to.equal(true)
+  })
+})
