fb71a67fd5f4807a6f74d8008b4720d285b68cd7
caldric <cvaldric@gmail.com>
2021-01-02 14:07:33 -0500

    Green: returns the correct value for second test

--- miniMaxSum.ts
+++ miniMaxSum.ts
@@ -1,3 +1,21 @@
-export const miniMaxSum = (arr: number[] = []): number[] => {
-  return [10, 14]
+export const miniMaxSum = (arr: number[] = []): [number, number] => {
+  // Initialize the minSum and maxSum variables
+  let minSum: number = Number.POSITIVE_INFINITY
+  let maxSum: number = Number.NEGATIVE_INFINITY
+
+  // Find the total sum
+  const totalSum: number = arr.reduce((sum, num) => sum + num)
+
+  // Iterate through the array
+  for (const num of arr) {
+    // Find the current sum
+    const currentSum: number = totalSum - num
+
+    // Compare the current sum with the min and max sums
+    minSum = Math.min(minSum, currentSum)
+    maxSum = Math.max(maxSum, currentSum)
+  }
+
+  // Return the min and max sums
+  return [minSum, maxSum]
 }
--- tests/miniMaxSum.spec.ts
+++ tests/miniMaxSum.spec.ts
@@ -7,4 +7,9 @@ describe('miniMaxSum', () => {
     const result = miniMaxSum([1, 2, 3, 4, 5])
     expect(result).to.have.same.members([10, 14])
   })
+
+  it('Returns [16, 24] with [1, 3, 5, 7, 9] passed in', () => {
+    const result = miniMaxSum([1, 3, 5, 7, 9])
+    expect(result).to.have.same.members([16, 24])
+  })
 })
