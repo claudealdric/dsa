15cdf0727d984fea54aecec6e0076534ee700ad0
caldric <cvaldric@gmail.com>
2021-01-02 13:51:22 -0500

    Red: miniMaxSum returns the correct value

--- tests/miniMaxSum.spec.ts
+++ tests/miniMaxSum.spec.ts
@@ -7,4 +7,9 @@ describe('miniMaxSum', () => {
     const result = miniMaxSum([])
     expect(result).to.have.same.members([])
   })
+
+  it('Returns [10, 14] with [1, 2, 3, 4, 5] passed in', () => {
+    const result = miniMaxSum([1, 2, 3, 4, 5])
+    expect(result).to.have.members([10, 14])
+  })
 })
