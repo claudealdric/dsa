43758a39e1e675d949a6042296c7826dfaa40f28
claudealdric <cvaldric@gmail.com>
2021-08-23 01:17:33 -0400

    Add linear time complexity solution

--- rwc/2021-08-16/longestPalindromicSubstring.spec.ts
+++ rwc/2021-08-16/longestPalindromicSubstring.spec.ts
@@ -1,12 +1,31 @@
-import quadraticSolution from './longestPalindromicSubstring';
+import {
+  linearSolution,
+  quadraticSolution,
+} from './longestPalindromicSubstring';
 
 describe('longestPalindromicSubstring', () => {
-  test.each([
+  const testCases = [
     ['aba', 'babad'],
     ['baab', 'baab'],
     ['baxabaxab', 'abaxabaxabb'],
     ['baxabybaxab', 'abaxabaxabybaxabyb'],
-  ])('returns "%s" with the arg "%s"', (expectedValue, stringArg) => {
-    expect(quadraticSolution(stringArg)).toBe(expectedValue);
+  ];
+
+  describe('quadratic time complexity solution', () => {
+    test.each(testCases)(
+      'returns "%s" with the arg "%s"',
+      (expectedValue, stringArg) => {
+        expect(quadraticSolution(stringArg)).toBe(expectedValue);
+      }
+    );
+  });
+
+  describe('linear time complexity solution', () => {
+    test.each(testCases)(
+      'returns "%s" with the arg "%s"',
+      (expectedValue, stringArg) => {
+        expect(linearSolution(stringArg)).toBe(expectedValue);
+      }
+    );
   });
 });
--- rwc/2021-08-16/longestPalindromicSubstring.ts
+++ rwc/2021-08-16/longestPalindromicSubstring.ts
@@ -23,7 +23,7 @@ const getValueAndIndexOfMaxElement = (numbers: number[]): [number, number] => {
   return [maxNumber, maxIndex];
 };
 
-const quadraticSolution = (s: string): string => {
+export const quadraticSolution = (s: string): string => {
   // The max radius of each palindromic substring centered on the corresponding index
   const palindromicSubstringRadii: number[] = new Array(s.length).fill(0);
 
@@ -60,4 +60,78 @@ const quadraticSolution = (s: string): string => {
   return longestPalindromicSubstring;
 };
 
-export default quadraticSolution;
+export const linearSolution = (s: string): string => {
+  // The max radius of each palindromic substring centered on the corresponding index
+  const palindromicSubstringRadii: number[] = new Array(s.length).fill(0);
+
+  // Add filler character between each character of the original string
+  // to handle both even and odd length cases
+  const fillerChar = '|';
+  const modifiedStr = addFillerCharacters(s, fillerChar);
+
+  // Initialize center and radius, since we can't optimize yet
+  let center = 0;
+  let radius = 0;
+
+  while (center < modifiedStr.length) {
+    // Find the longest palindromic substring radius for the current center
+    while (
+      center - radius - 1 >= 0 && // left pointer lookahead
+      center + radius + 1 < modifiedStr.length && // right pointer lookahead
+      modifiedStr[center - radius - 1] === modifiedStr[center + radius + 1]
+    ) {
+      radius += 1;
+    }
+
+    palindromicSubstringRadii[center] = radius;
+
+    const oldCenter = center;
+    const oldRadius = radius;
+    center += 1;
+
+    // Apply optimizations when the new center is within the old palindrome
+    while (center <= oldRadius + oldCenter) {
+      const mirroredCenter = oldCenter - (center - oldCenter);
+      const maxMirroredRadius = oldCenter + oldRadius - center;
+
+      if (
+        (palindromicSubstringRadii[mirroredCenter] as number) <
+        maxMirroredRadius
+      ) {
+        // If the mirrored palindrome is fully contained within the old palindrome,
+        // then the radius of the new center is the same as the radius of the mirrored center
+        palindromicSubstringRadii[center] = palindromicSubstringRadii[
+          mirroredCenter
+        ] as number;
+        center += 1;
+      } else if (
+        (palindromicSubstringRadii[mirroredCenter] as number) >
+        maxMirroredRadius
+      ) {
+        // If the mirrored palindrome extends beyond the left boundary of the old palindrome,
+        // then the radius of the new center cannot extend beyond the right boundary
+        // of the old palindrome
+        palindromicSubstringRadii[center] = maxMirroredRadius;
+        center += 1;
+      } else {
+        // If the mirrored palindrome extends exactly to the left boundary of the old palindrome,
+        // then the radius of the new center is at least the radius of the mirrored center.
+        // The character(s) after the old palindrome might make the new palindrome longer.
+        radius = palindromicSubstringRadii[mirroredCenter] as number;
+        break;
+      }
+    }
+  }
+
+  const [maxRadius, maxRadiusIndex] = getValueAndIndexOfMaxElement(
+    palindromicSubstringRadii
+  );
+
+  const longestPalindromicSubstring = modifiedStr
+    .slice(maxRadiusIndex - maxRadius, maxRadiusIndex + maxRadius + 1)
+    .split('')
+    .filter((char) => char !== fillerChar)
+    .join('');
+
+  return longestPalindromicSubstring;
+};
