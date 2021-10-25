1c821fc940083285567222f63d35e517bade0091
claudealdric <cvaldric@gmail.com>
2021-10-25 02:11:47 -0400

    Add comments

--- rwc/2021-10-18/reorder.ts
+++ rwc/2021-10-18/reorder.ts
@@ -11,24 +11,29 @@ export function reorderCheck<T>(elements: T[], indices: number[]): T[] {
 }
 
 export function reorder<T>(elements: T[], indices: number[]): T[] {
+  // Start with checking the 0th element
   let currentIndex = 0;
-  let targetIndex = indices[0];
 
   for (let iteration = 0; iteration < elements.length - 1; iteration += 1) {
+    // Keep a while loop counter
     let whileIterations = 0;
+    let targetIndex = indices[currentIndex] as number;
 
     while (currentIndex === targetIndex) {
+      // Move to the next element if the current element is already in the right order
+      // Let the next element be the first element if the previous element is the last
       currentIndex = (currentIndex + 1) % elements.length;
-      targetIndex = indices[currentIndex];
+      targetIndex = indices[currentIndex] as number;
 
       whileIterations += 1;
 
       if (whileIterations + 1 >= elements.length) {
+        // Return the elements array if the order has been correct for one complete loop
         return elements;
       }
     }
 
-    targetIndex = indices[currentIndex] as number;
+    // Perform reordering swaps
     [elements[currentIndex], elements[targetIndex]] = [
       elements[targetIndex] as T,
       elements[currentIndex] as T,
