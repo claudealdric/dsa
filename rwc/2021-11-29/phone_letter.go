f79e1bca922779b3598f7e3112e22fa6b8550e8f
claudealdric <cvaldric@gmail.com>
2021-11-29 19:25:45 -0500

    Switch to byte slice instead of string builder

--- rwc/2021-11-29/phone_letter.go
+++ rwc/2021-11-29/phone_letter.go
@@ -2,32 +2,31 @@ package phoneLetter
 
 import (
 	"fmt"
-	"strings"
 )
 
 func GetLetterCombinations(digits string) []string {
-	var combinations [][]string
-	digitsToLetters := map[string][]string{
-		"2": {"a", "b", "c"},
-		"3": {"d", "e", "f"},
-		"4": {"g", "h", "i"},
-		"5": {"j", "k", "l"},
-		"6": {"m", "n", "o"},
-		"7": {"p", "q", "r", "s"},
-		"8": {"t", "u", "v"},
-		"9": {"w", "x", "y", "z"},
+	var combinations [][]byte
+	digitsToLetters := map[byte][]byte{
+		'2': {'a', 'b', 'c'},
+		'3': {'d', 'e', 'f'},
+		'4': {'g', 'h', 'i'},
+		'5': {'j', 'k', 'l'},
+		'6': {'m', 'n', 'o'},
+		'7': {'p', 'q', 'r', 's'},
+		'8': {'t', 'u', 'v'},
+		'9': {'w', 'x', 'y', 'z'},
 	}
 
 	for i := 0; i < len(digits); i++ {
-		digit := string(digits[i])
+		digit := digits[i]
 		lettersForDigit := digitsToLetters[digit]
-		var newCombinationsAfterNewDigit [][]string
+		var newCombinationsAfterNewDigit [][]byte
 
 		digitIsTheFirst := i == 0
 
 		if digitIsTheFirst {
 			for _, letterForDigit := range lettersForDigit {
-				combinations = append(combinations, []string{letterForDigit})
+				combinations = append(combinations, []byte{letterForDigit})
 			}
 			continue
 		}
@@ -45,18 +44,13 @@ func GetLetterCombinations(digits string) []string {
 		combinations = newCombinationsAfterNewDigit
 	}
 
-	return mapStringSliceToString(combinations)
+	return mapByteSliceToString(combinations)
 }
 
-func mapStringSliceToString(input [][]string) []string {
+func mapByteSliceToString(input [][]byte) []string {
 	var transformed []string
-
-	for _, stringSlice := range input {
-		var stringBuilder strings.Builder
-		for _, char := range stringSlice {
-			stringBuilder.WriteString(char)
-		}
-		transformed = append(transformed, stringBuilder.String())
+	for _, byteSlice := range input {
+		transformed = append(transformed, string(byteSlice))
 	}
 
 	return transformed
