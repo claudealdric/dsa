35168cc3ee9a1ebe82450b19d0e06bff422517d9
claudealdric <cvaldric@gmail.com>
2021-12-05 02:53:10 -0500

    Change test name casing

--- aoc/day-1/depth-increase-count.spec.ts
+++ aoc/day-1/depth-increase-count.spec.ts
@@ -1,7 +1,7 @@
 import depthIncreaseCount from './depth-increase-count';
 
 describe('depthIncrease', () => {
-  it('Returns the correct number of increases', () => {
+  it('returns the correct number of increases', () => {
     expect(
       depthIncreaseCount([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
     ).toBe(7);
