5846f091a2e605b08d2352c080a435098b495de1
claudealdric <cvaldric@gmail.com>
2022-05-01 09:39:20 -0400

    Write unit tests

---
+++ rwc/2022-04-25/merge-intervals.spec.ts
@@ -0,0 +1,42 @@
+import { Interval, mergeIntervals } from './merge-intervals';
+
+describe('mergeIntervals', () => {
+  const testCases: Array<[Interval[], Interval[]]> = [
+    [
+      [
+        [1, 6],
+        [8, 10],
+        [15, 20],
+      ],
+      [
+        [1, 4],
+        [2, 6],
+        [8, 10],
+        [15, 20],
+      ],
+    ],
+    [
+      [[1, 7]],
+      [
+        [1, 2],
+        [2, 7],
+      ],
+    ],
+    [
+      [
+        [1, 2],
+        [3, 7],
+      ],
+      [
+        [1, 2],
+        [3, 7],
+      ],
+    ],
+  ];
+  test.concurrent.each(testCases)(
+    'merges intervals that overlap',
+    async (expected, input) => {
+      expect(mergeIntervals(input)).toStrictEqual(expected);
+    }
+  );
+});
