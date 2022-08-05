cdb4074927d1b4e935b62100a3cb77a828405ea6
claudealdric <cvaldric@gmail.com>
2022-08-05 00:24:07 -0400

    Create a singly linked list data structure

---
+++ data-structures/singly-linked-list.ts
@@ -0,0 +1,9 @@
+export class LinkedList {
+  val: number | null;
+  next: LinkedList | null;
+
+  constructor(val?: number, next?: LinkedList) {
+    this.val = val ?? null;
+    this.next = next ?? null;
+  }
+}
