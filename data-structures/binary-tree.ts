7821049d69090e2f06b8a0e9d3fb220a1e546000
claudealdric <cvaldric@gmail.com>
2022-08-05 00:44:33 -0400

    Include null to binary tree node args

--- data-structures/binary-tree.ts
+++ data-structures/binary-tree.ts
@@ -3,7 +3,7 @@ export class Node {
 	left: Node | null;
 	right: Node | null;
 
-	constructor(val?: number, left?: Node, right?: Node) {
+	constructor(val?: number, left?: Node | null, right?: Node | null) {
 		this.val = val ?? null;
 		this.left = left ?? null;
 		this.right = right ?? null;
